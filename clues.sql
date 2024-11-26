ac32ae8d1b027d1968be44ba30e53ebe4d60e8c8
eeexuanlyx <eeexuanlyx@gmail.com>
2024-11-26 21:31:06 +0800

    complete lab

--- clues.sql
+++ clues.sql
@@ -1,25 +1,21 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
--- Write SQL query here
 
 SELECT * FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
--- Write SQL query here
 
 SELECT * FROM countrylanguages WHERE countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
--- Write SQL query here
 
 SELECT * FROM countrylanguages INNER JOIN countries ON countrylanguages.countrycode = countries.code WHERE region = 'Southern Europe' AND language = 'Italian';
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
--- Write SQL query here
 
  SELECT * FROM cities WHERE name IN('Italy', 'San Marino', 'Holy See (Vatican City State');
  SELECT name from cities WHERE countrycode = 'SMR';
@@ -28,7 +24,6 @@ SELECT * FROM countrylanguages INNER JOIN countries ON countrylanguages.countryc
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
--- Write SQL query here
 
 
 SELECT countrycode FROM cities WHERE name ~ 'Serra' AND countrycode IN(SELECT code FROM countries WHERE region = 'South America');
@@ -38,7 +33,6 @@ SELECT countrycode FROM cities WHERE name ~ 'Serra' AND countrycode IN(SELECT co
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
--- Write SQL query here
 
  SELECT name from countries WHERE code = 'BRA';
 
